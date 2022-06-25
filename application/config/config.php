33dd2a6c6ff168035081c167045bf10a8c6838e2
HV9903 <108178498+HV9903@users.noreply.github.com>
2022-06-25 16:50:00 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'sdms.github.io';
+$config['base_url'] = 'http://localhost/SDMS/';
 
 /*
 |--------------------------------------------------------------------------
